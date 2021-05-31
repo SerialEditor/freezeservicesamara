617c3633a783bb0fc850641f05b4061a4512129e
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2021-05-31 20:25:24 +0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let contacts = document.querySelector('.contacts');
-
-contacts.onclick = function () {
-    contacts.classList.toggle('contacts-open');
-    contacts.classList.toggle('contacts-close');
-};
